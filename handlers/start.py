7f0a24a1c013366011be25433152f6dda98a30ac
GracelandP <adamalhashush@mail.ru>
2023-12-12 17:52:36 +0600

    Добавление кнопки Входа/Регистрации

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,11 @@
 from aiogram import F, Bot, Dispatcher, Router, types
 from aiogram.filters import Command
 from aiogram.types import Message, FSInputFile, CallbackQuery
+from keyboards.startkey import sign
 
 router = Router()
 
 @router.message(Command("start"))
 async def start(message: Message):
     photo = FSInputFile("media/start.jpg")
-    await message.answer_photo(photo, caption = f'Привет! Я – чат-бот "EngTeachBot", помогу вам с изучением английских слов, с помощью специально разработанных тренажеров.')
+    await message.answer_photo(photo, caption = f'Привет! Я – чат-бот "EngTeachBot", помогу вам с изучением английских слов, с помощью специально разработанных тренажеров.', reply_markup=sign())
